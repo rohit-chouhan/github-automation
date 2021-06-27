da8798daec9103e519846c08f98b90c34cbad23e
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2021-06-27 19:16:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,43 +7,43 @@ import requests
 
 def follow(t):
     page = 1
-    utha = 0
+    indexing = 0
     limit=int(input("\n Enter Follow Limit: "))
     user=int(input(" Enter The User name of Person to Follow their Followers: "))
     for i in range(limit):
         response = requests.get('https://api.github.com/users/'+user+'/followers?page='+str(page), headers= {'Authorization' : 'Bearer '+t+''})
         res = response.json()
-        requests.put('https://api.github.com/user/following/'+res[utha]['login'], headers= {'Authorization' : 'Bearer '+t+''})
-        print(" ("+str(i+1)+") Followed User Successfully: @"+res[utha]['login'])
-        if utha==29:
+        requests.put('https://api.github.com/user/following/'+res[indexing]['login'], headers= {'Authorization' : 'Bearer '+t+''})
+        print(" ("+str(i+1)+") Followed User Successfully: @"+res[indexing]['login'])
+        if indexing==29:
             page+=1
-            utha=0
+            indexing=0
         elif i==limit:
             break
         else:
             pass
-        utha+=1   
+        indexing+=1   
 
     print("\n\n===== Task Complete Successfully =====")
     print("\n\nNote: Github takes 5-10min to update!")
     
 def unfollow(u,t):
     page = 1
-    utha = 0
+    indexing = 0
     limit=int(input("\n Enter UnFollow Limit: "))
     for i in range(limit):
         response = requests.get('https://api.github.com/users/'+u+'/following?page='+str(page), headers= {'Authorization' : 'Bearer '+t+''})
         res = response.json()
-        requests.delete('https://api.github.com/user/following/'+res[utha]['login'], headers= {'Authorization' : 'Bearer '+t+''})
-        print(" ("+str(i+1)+") Unfollowed User Successfully: @"+res[utha]['login'])
-        if utha==29:
+        requests.delete('https://api.github.com/user/following/'+res[indexing]['login'], headers= {'Authorization' : 'Bearer '+t+''})
+        print(" ("+str(i+1)+") Unfollowed User Successfully: @"+res[indexing]['login'])
+        if indexing==29:
             page+=1
-            utha=0
+            indexing=0
         elif i==limit:
             break
         else:
             pass
-        utha+=1   
+        indexing+=1   
 
     print("\n\n===== Task Complete Successfully =====")
     print("\n\nNote: Github takes 5-10min to update!")
